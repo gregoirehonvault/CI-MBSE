9f46314ebbaeddce499bc3112acf86c0fb1f0aa9
Gregoire Honvault <124054408+gregoirehonvault@users.noreply.github.com>
2023-08-28 16:44:02 +0200

    Update run.py

--- CIInterface/run.py
+++ CIInterface/run.py
@@ -4,15 +4,9 @@
 import gradio as gr
 
 
-
-f = open("c:/Users/Gregh/OneDrive/Bureau/STAGE/md.txt", "r")
-content = f.read()
-
 with gr.Blocks() as demo:
     gr.Markdown(value='This _example_ was **written** in [Markdown](https://en.wikipedia.org/wiki/Markdown)\n')
 
-with gr.Blocks() as demo:
-    gr.Markdown(value=content)
 
 #share=True
 demo.launch()
